fbf2a5455efca724880ad3f20d0155bc07054a05
Luma Development <12071794+LumaDevelopment@users.noreply.github.com>
2021-11-24 00:18:58 -0500

    Update Main.java

--- src/me/alphagladiator/kpe/Main.java
+++ src/me/alphagladiator/kpe/Main.java
@@ -7,12 +7,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 	
-	public static Main instance;
-	
-	public static Main getInstance(){
-		return instance;
-	}
-	
 	public void registerClasses(){
 		PluginManager pm = Bukkit.getPluginManager();
 		pm.registerEvents(new EventListener(), this);
@@ -25,19 +19,15 @@ public class Main extends JavaPlugin{
 	
 	@Override
 	public void onEnable(){
-	    PluginDescriptionFile pdf = getDescription();
+		PluginDescriptionFile pdf = getDescription();
 		consoleBroadcast("[" + pdf.getName() + "] " + pdf.getName() + " v" + pdf.getVersion() + " by " + pdf.getAuthors() + " has been enabled.");
-		consoleBroadcast("Description: " + pdf.getDescription());
 		registerClasses();
-		instance = this;
 	}
 	
 	@Override
 	public void onDisable(){
 		PluginDescriptionFile pdf = getDescription();
 		consoleBroadcast("[" + pdf.getName() + "] " + pdf.getName() + " v" + pdf.getVersion() + " by " + pdf.getAuthors() + " has been disabled.");
-		consoleBroadcast("Description: " + pdf.getDescription());
-		instance = null;
 	}
 
 }
